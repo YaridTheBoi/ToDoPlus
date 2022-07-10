004766af08cf3d7a8c3ac316313ced07a10b810e
YaridTheBoi <kacper.chrostowski@gmail.com>
2022-07-11 00:04:09 +0200

    created some requests for tasks

--- to_do_plus/api/urls.py
+++ to_do_plus/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import main, TaskView
+from .views import main, TaskView, getAllTasks, removeTaskById, createTask
 
 
 urlpatterns = [
     path('', main),
     path('home', main),
-    path('api', TaskView.as_view())
+    path('api', TaskView.as_view()),
+    path('allTasks', getAllTasks),
+    path('removeTask/<int:id>', removeTaskById),
+    path('newTask', createTask)
 ]
--- to_do_plus/api/views.py
+++ to_do_plus/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -12,3 +14,28 @@ def main(request):
 class TaskView (generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class= TaskSerializer
+
+@api_view(['GET'])
+def getAllTasks(request):
+    tasks= Task.objects.all()
+    serializer=TaskSerializer(tasks, many=True)
+    return(Response(serializer.data))
+
+@api_view(['DELETE'])
+def removeTaskById(request, id):
+    try:
+        task= Task.objects.get(id=id)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    task.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def createTask(request):
+    serializer=TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
