17030b80fd387238c5a7011a3e0c626066907c0c
YaridTheBoi <kacper.chrostowski@gmail.com>
2022-07-11 23:09:50 +0200

    chagned from func view to class view

--- to_do_plus/api/urls.py
+++ to_do_plus/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import main, TaskView, getAllTasks, removeTaskById, createTask
-
+from .views import main, TaskList, TaskDetailed
 
 urlpatterns = [
     path('', main),
     path('home', main),
-    path('api', TaskView.as_view()),
-    path('allTasks', getAllTasks),
-    path('removeTask/<int:id>', removeTaskById),
-    path('newTask', createTask)
+    #path('api', TaskView.as_view()),
+    path('myTasks', TaskList.as_view()),
+    path('myTasks/<int:id>', TaskDetailed.as_view())
+
 ]
--- to_do_plus/api/views.py
+++ to_do_plus/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import generics, status
-from rest_framework.decorators import api_view
+from django.http import HttpResponse, Http404
+from rest_framework import generics, status, mixins
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -11,15 +11,57 @@ from .serializers import TaskSerializer
 def main(request):
     return HttpResponse("Main Page")
 
-class TaskView (generics.CreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class= TaskSerializer
+class TaskList (generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    
+    serializer_class=TaskSerializer
 
-@api_view(['GET'])
-def getAllTasks(request):
-    tasks= Task.objects.all()
-    serializer=TaskSerializer(tasks, many=True)
-    return(Response(serializer.data))
+    def get_queryset(self):
+        queryset=Task.objects.all()
+        author_id = self.request.query_params.get('author_id')
+        if author_id is not None:
+            queryset=queryset.filter(author_id=author_id)
+        return queryset
+
+
+    def get(self, request):
+        return self.list(request)
+
+
+    #https://stackoverflow.com/questions/42346200/django-rest-add-data-to-serializer-when-saving przyda sie przy tworzeniu posta
+    def post(self, request):
+        return self.create(request)
+
+
+class TaskDetailed(generics.GenericAPIView):
+    queryset=Task.objects.all()
+    serializer_class=TaskSerializer
+
+
+    def get_task_by_id(self, id):
+        try:
+            return Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id, format=None):
+        tasks= self.get_task_by_id(id)
+        serializer=TaskSerializer(tasks)
+        return Response(serializer.data)
+
+    def delete(self, request, id, format=None):
+        task=self.get_task_by_id(id)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id, format=None):
+        task=self.get_task_by_id(id)
+        serializer=TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''
+
 
 @api_view(['DELETE'])
 def removeTaskById(request, id):
@@ -32,10 +74,14 @@ def removeTaskById(request, id):
     task.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+
+
+
 @api_view(['POST'])
 def createTask(request):
     serializer=TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)'''
