d6e1ee1a631e275bcf8e2692f007f53e209b6024
YaridTheBoi <kacper.chrostowski@gmail.com>
2022-08-03 10:49:49 +0200

    displaying tasks in mytask

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -4,6 +4,33 @@ function getTasks(){
         return response.json()
     }).then((data)=>{
         console.log(data);
+        displayTasks(data);
     })
 
+}
+
+function displayTasks(data){
+    for (var i=0; i<data.length; i++){
+        var element=document.getElementById("all-tasks")
+        var task=document.createElement("div")
+
+        var title=document.createElement("H1")
+        var title_text=document.createTextNode(data[i].title)
+        var description=document.createTextNode("description: "+ data[i].description)
+        var date=document.createTextNode("created: "+ data[i].create_date)
+        var p=document.createElement("p")
+        var p2=document.createElement("p")
+
+        title.appendChild(title_text)
+        task.appendChild(title)
+
+        p.appendChild(description)
+        task.appendChild(p)
+        
+        p2.appendChild(date)
+        task.appendChild(p2)
+
+
+        element.appendChild(task)
+    }
 }
