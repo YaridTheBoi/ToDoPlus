ebfc0edf629af35d93a2c3d48d9cb0afc7d4edd5
YaridTheBoi <kacper.chrostowski@gmail.com>
2022-08-17 14:58:44 +0200

    created popup for password recovery

--- Frontend/js/forgotPassword.js
+++ Frontend/js/forgotPassword.js
@@ -1,10 +1,14 @@
 const btn=document.getElementById("forgotPassword-form")
 const formDiv=document.getElementById("mainWindow")
+const succPop=document.getElementById("succPop")
+succPop.style.display="none"
+formDiv.style.display="flex"
 btn.addEventListener("submit", function(e){
     e.preventDefault()
     ForgotPasswordClick()
 })
 
 function ForgotPasswordClick(){
-    formDiv.style.display="none"
+    formDiv.style.opacity="0"
+    succPop.style.display="flex"
 }
